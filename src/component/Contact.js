2d38be477063dba69a2558ef7b50204eaf8c034d
TankDodi16 <tankdodi@gmail.com>
2022-01-18 10:29:55 +0530

    minor updates

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -15,7 +15,7 @@ class ContactUs extends Component {
       const contactStyle = {
         backgroundImage: `url("https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80")` ,
         height:'90vh',
-        width: '213.5vh',
+        width: '231.5vh',
         marginTop:'-26.5px',
         fontSize:'20px',
         backgroundSize: 'cover',
@@ -28,7 +28,6 @@ class ContactUs extends Component {
         color: '#87ceeb',
         position: 'relative',
         top: '70px',
-        left: '46.60%'
       }
       
       // console.log(this.props);
@@ -39,7 +38,7 @@ class ContactUs extends Component {
       return ( 
         <div style={contactStyle}>
                 <h1 style={font}>Welcome to Contact Us Page. </h1>
-                <h3 style={fontColor}>The data:{this.props.location.state}. </h3>
+                <h3 style={fontColor}>The data: {this.props.location.state}.</h3>
                 <Link className="backBtn" to='/'>Go back</Link>
             </div>
          );
